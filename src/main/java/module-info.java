b228851ae2845dfab5ebc82f1eaf1dc30e8d2d5e
MatheusHMR <matheus.henrimr@gmail.com>
2023-07-02 23:35:57 -0300

    feat added missing structures for module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module trabalhopoov {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens trabalhopoov to javafx.fxml;
+    opens trabalhopoov.controllers.preparacao to javafx.fxml, javafx.graphics;
     exports trabalhopoov;
 }
