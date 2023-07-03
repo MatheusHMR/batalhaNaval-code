b82f4b6415d631e319848987d1b506b3d43e76f0
MatheusHMR <matheus.henrimr@gmail.com>
2023-07-02 23:36:15 -0300

    feat Finished Parte Class

--- src/main/java/trabalhopoov/model/Parte.java
+++ src/main/java/trabalhopoov/model/Parte.java
@@ -4,7 +4,16 @@ public class Parte extends Peca{
 
     private Navio navio;
 
-    public Parte(Navio navio) {
+    public Parte(int posicaox, int posicaoy, char identificador, Navio navio) {
+        super(posicaox, posicaoy, identificador);
+        this.navio = navio;
+    }
+
+    public Navio getNavio() {
+        return navio;
+    }
+
+    public void setNavio(Navio navio) {
         this.navio = navio;
     }
 
